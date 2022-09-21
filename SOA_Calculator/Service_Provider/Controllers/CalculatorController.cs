54d08e7310e49241be673e8e9b1631087aa1edf2
sharronfoo <wks.foo@gmail.com>
2022-09-21 13:32:28 +0800

    chore: added CalculatorController witth attribute routing

---
+++ SOA_Calculator/Service_Provider/Controllers/CalculatorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Service_Provider.Controllers
+{
+    [RoutePrefix("api/calculator")]
+    public class CalculatorController : ApiController
+    {
+        [Route("ADDTwoNumbers/{firstNumber}/{secondNumber}")]
+        [Route("ADDTwoNumbers")]
+        [HttpGet]
+        public int ADDTwoNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        [Route("ADDThreeNumbers/{firstNumber}/{secondNumber}/{thirdNumber}")]
+        [Route("ADDThreeNumbers")]
+        [HttpGet]
+        public int ADDThreeNumbers(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return firstNumber + secondNumber + thirdNumber;
+        }
+
+        [Route("MulTwoNumbers/{firstNumber}/{secondNumber}")]
+        [Route("MulTwoNumbers")]
+        [HttpGet]
+        public int MulTwoNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        [Route("MulThreeNumbers/{firstNumber}/{secondNumber}/{thirdNumber}")]
+        [Route("MulThreeNumbers")]
+        [HttpGet]
+        public int MulThreeNumbers(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return firstNumber * secondNumber * thirdNumber;
+        }
+
+    }
+}
