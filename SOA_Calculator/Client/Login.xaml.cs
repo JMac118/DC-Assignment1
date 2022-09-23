01259782e0cb41b6c47d43d961974ac91d6043c5
sharronfoo <wks.foo@gmail.com>
2022-09-23 20:20:47 +0800

    chore: created Login Window

--- SOA_Calculator/Client/Login.xaml.cs
+++ SOA_Calculator/Client/Login.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +23,51 @@ namespace Client
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (EmailTextBox.Text.Length == 0)
+            {
+                EmailErrorText.Text = "Email field must be filled";
+                EmailTextBox.Focus();
+            }
+            else if (!Regex.IsMatch(EmailTextBox.Text, @"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$"))
+            {
+                EmailErrorText.Text = "Enter a valid email, e.g. example@email.com";
+                EmailTextBox.Select(0, EmailTextBox.Text.Length);
+                EmailTextBox.Focus();
+            }
+            else
+            {
+                EmailErrorText.Text = "";
+
+                if (PasswordTextBox.Text.Length == 0)
+                {
+                    PasswordErrorText.Text = "Password field must be filled";
+                    PasswordTextBox.Focus();
+                }
+                else
+                {
+                    PasswordErrorText.Text = "";
+
+                    //TODO: send credentials to Authentication Service
+                }
+            }
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow home = new MainWindow();
+            home.Show();
+            Close();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            Registration registration = new Registration();
+            registration.Show();
+            Close();
         }
     }
 }
